597d623820017aae74315a97f84c90480e48b95c
donaldjclark <donaldjclark@gmail.com>
2025-10-25 15:21:39 -0400

    Change base path for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/4theone-international-site/', // <- MUST MATCH YOUR REPO NAME
+  base: './', // <- MUST MATCH YOUR REPO NAME
   plugins: [react()],
   build: {
     outDir: 'docs' // <- so GitHub Pages can find it
