0bc5f69c60bf1bdc68b7ab68a87ede4c4764377f
donaldjclark <donaldjclark@gmail.com>
2025-12-27 08:54:56 -0500

    Fix Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/', // <- Root path for custom domain
+  base: '/4theone-international-site/', // <- GitHub Pages subpath
   plugins: [react()],
   build: {
     outDir: 'docs' // <- so GitHub Pages can find it
